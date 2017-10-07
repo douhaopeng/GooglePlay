c611a1393eafa6014a1ca02d23da48dac1cad25f
Administrator <1591994251@qq.com>
2017-10-07 09:52:21 +0800

    first

--- app/src/main/java/com/example/douhaopeng/googleplay/com/example/douhaopeng/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/douhaopeng/googleplay/com/example/douhaopeng/ui/fragment/HomeFragment.java
@@ -1,27 +1,77 @@
 package com.example.douhaopeng.googleplay.com.example.douhaopeng.ui.fragment;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.douhaopeng.googleplay.R;
+import com.example.douhaopeng.googleplay.com.example.douhaopeng.ui.activity.BaseActivity;
 import com.example.douhaopeng.googleplay.com.example.douhaopeng.ui.view.LoadingPage;
 import com.example.douhaopeng.googleplay.com.example.douhaopeng.utils.UIUtils;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2017\10\6 0006.
  */
 
 public class HomeFragment extends BaseFragment {
+    private ArrayList<String> data;
     //如果加载数据成功就回调此方法，运行在主线程
     @Override
     public View onCreateSuccessView() {
-        TextView view = new TextView(UIUtils.getContext());
-        view.setText(getClass().getSimpleName());
+//        TextView view = new TextView(UIUtils.getContext());
+//        view.setText(getClass().getSimpleName());
+        ListView view =new ListView(UIUtils.getContext());
+        view.setAdapter(new HomeAdapter());
         return view;
     }
     //已经运行在子线程，可以直接执行耗时操作
     @Override
     public LoadingPage.ResultState onLoad() {
         //请求网络
+        data = new ArrayList<String>();
+        for (int i=0;i<20;i++){
+            data.add("测试数据"+i);
+        }
         return LoadingPage.ResultState.STATE_SUCCESS;
+    }
+    class HomeAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public String getItem(int i) {
+            return data.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder holder;
+            if(view==null){
+                view = UIUtils.inflate(R.layout.list_item_home);
+                holder = new ViewHolder();
+                holder.tvContent = view.findViewById(R.id.tv_content);
+                view.setTag(holder);
+            }else{
+               holder = (ViewHolder) view.getTag();
+            }
+            String content =getItem(i);
+            holder.tvContent.setText(content);
+            return view;
+        }
+    }
+    static class ViewHolder{
+        public TextView tvContent;
     }
 }
